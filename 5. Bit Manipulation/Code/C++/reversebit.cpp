a56e40d0c0a788610c306fbd15bfdace74b83a36
Abhishek1180 <Abhishek11801529@gmail.com>
2021-03-28 18:25:09 +0530

    init

--- 5. Bit Manipulation/Code/C++/reversebit.cpp
+++ 5. Bit Manipulation/Code/C++/reversebit.cpp
@@ -6,16 +6,16 @@ U setbit(U N, U pos) { return N|(1<<pos);}
 U unsetbit(U N, U pos) { return N&(~(1<<pos));}
 U reversebit(U N)
 {
-    U Msb = sizeof(N)*8-1 , Lsb = 0;
-    while(Msb>Lsb)
+    U Msb = sizeof(N)*8-1 , Lsb = 0;  //msb bits 31
+    while(Msb>Lsb) //till msb pos is greater then lsb
     {
-        U Mbit = checkbit(N,Msb) , Lbit = checkbit(N,Lsb);
-        if(Mbit!=Lbit)
+        U Mbit = checkbit(N,Msb) , Lbit = checkbit(N,Lsb); //check msb and lsb 
+        if(Mbit!=Lbit) // we will only swap if msb and lsb bcz if same no swap is required.
         {
-            N = Mbit?setbit(N,Lsb):unsetbit(N,Lsb);
-            N = Lbit?setbit(N,Msb):unsetbit(N,Msb);
+            N = Mbit?setbit(N,Lsb):unsetbit(N,Lsb); // if msb is 1 then unset it or viceversa
+            N = Lbit?setbit(N,Msb):unsetbit(N,Msb); // if lsb is 1 then unset it or viceversa
         }
-        Msb--;Lsb++;
+        Msb--;Lsb++; //inc lsb and dcr msb
     }
     return N;
 }
