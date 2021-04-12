ff010c10dc1d5fc97c7e15fc2ca82641785cce43
Abhishek1180 <Abhishek11801529@gmail.com>
2021-04-12 23:55:48 +0530

    init

--- 2. Time Complexity-Recursive Programs/Code/C++/Fibonacci.cpp
+++ 2. Time Complexity-Recursive Programs/Code/C++/Fibonacci.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int fib(int x)
 {
     if(x<=1) return x;
+    if(x==2) return 1;
     return fib(x-1)+fib(x-2);
 }
 int main()
 {
-    for(int i=1;i<=13;i++) cout<<i<<" : "<<fib(i)<<endl;
+    for(int i=0;i<=13;i++) cout<<i<<" : "<<fib(i)<<endl;
 }
