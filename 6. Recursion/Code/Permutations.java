3156496a56f431c21860d320a9662695bcc29997
Abhishek1180 <Abhishek11801529@gmail.com>
2021-04-14 19:55:48 +0530

    init

---
+++ 6. Recursion/Code/Permutations.java
@@ -0,0 +1,107 @@
+// Permutations
+// Problem Description
+
+// Given an integer array A of size N denoting collection of numbers , return all possible permutations.
+
+// NOTE:
+
+// No two entries in the permutation sequence should be the same.
+// For the purpose of this problem, assume that all the numbers in the collection are unique.
+// WARNING: DO NOT USE LIBRARY FUNCTION FOR GENERATING PERMUTATIONS. Example : next_permutations in C++ / itertools.permutations in python.
+// If you do, we will disqualify your submission retroactively and give you penalty points.
+
+
+// Problem Constraints
+// 1 <= N <= 9
+
+
+
+// Input Format
+// Only argument is an integer array A of size N.
+
+
+
+// Output Format
+// Return a 2-D array denoting all possible permutation of the array.
+
+
+
+// Example Input
+// A = [1, 2, 3]
+
+
+// Example Output
+// [ [1, 2, 3]
+//   [1, 3, 2]
+//   [2, 1, 3] 
+//   [2, 3, 1] 
+//   [3, 1, 2] 
+//   [3, 2, 1] ]
+
+
+// Example Explanation
+// All the possible permutation of array [1, 2, 3].
+
+//Duplicates are allowed
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Permutations
+{
+    private ArrayList<ArrayList<Integer>> ans;
+    private void swap(ArrayList<Integer> A, int start, int end)
+    {
+        int temp = A.get(start);
+        A.set(start,A.get(end));
+        A.set(end,temp);
+    }
+    private void permutation( ArrayList<Integer> A, int start, int end)
+    {
+        if(start==end) ans.add(new ArrayList<Integer>(A)); //providing here a new instance of A so that it has no relation to previous A;
+        else
+        {
+            for(int i=start; i<=end; i++)
+            {
+                swap(A,start,i);
+                permutation(A,start+1,end);
+                swap(A,start,i);
+            }
+        }
+    }
+    ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A)
+    {
+        ans = new ArrayList<ArrayList<Integer>>();
+        permutation(A,0,A.size()-1);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Permutations num = new Permutations();
+        ArrayList<ArrayList<Integer>> P = num.permute(new ArrayList<Integer>(Arrays.asList(1,2,3,4)));
+        for(int i=0; i<P.size(); i++)
+        {
+            for(int j=0; j<P.get(i).size(); j++)
+            {
+                System.out.print(P.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }        
+    }
+}
+/**
+ * 
+ * 
+ *       Use recursion tree
+ * 
+ * 
+ * 
+ *                                                     A B C
+ *                                                       |
+ *                            .--------------------------.--------------------------.   
+ *                            |                          |                          |             
+ *                         [A] B C                    [B] A C                    [C] B A                  
+ *                        SWAP(A,A)                  SWAP(A,B)                  SWAP(A,C)
+ *                            |                          |                          |         
+ *                    .---------------.          .---------------.          .--------------.       
+ *                 [A B] C         [A C] B    [B A] C         [B C] A    [C B] A        [C A] B   
+ *                SWAP(B,B)       SWAP(B,C)  SWAP(A,A)       SWAP(A,C)  SWAP(B,B)      SWAP(B,A)
+ */                                 
