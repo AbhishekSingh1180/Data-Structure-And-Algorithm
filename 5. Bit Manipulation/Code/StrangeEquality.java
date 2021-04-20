96599484cc1b27e20fe9c50b15fdbf0ad6e00c63
Abhishek1180 <Abhishek11801529@gmail.com>
2021-04-20 19:01:30 +0530

    init

--- 5. Bit Manipulation/Code/StrangeEquality.java
+++ 5. Bit Manipulation/Code/StrangeEquality.java
@@ -1,8 +1,59 @@
+// Strange Equality
+// Problem Description
+
+// Given an integer A.
+// Two numbers X and Y are defined as follows:
+
+// X is the greatest number smaller than A such that XOR sum of X and A is the same as the sum of X and A.
+// Y is the smallest number greater than A such that XOR sum of Y and A is the same as the sum of Y and A.
+// Find and return the XOR of X and Y.
+
+// NOTE 1: XOR of X and Y is defined as X ^ Y where '^' is the BITWISE XOR operator.
+
+// NOTE 2: Your code will be run against a maximum of 100000 Test Cases.
+
+
+
+// Problem Constraints
+// 1 <= A <= 109
+
+
+
+// Input Format
+// First and only argument is an integer A.
+
+
+
+// Output Format
+// Return an integer denoting the XOR of X and Y.
+
+
+
+// Example Input
+// A = 5
+
+
+// Example Output
+// 10
+
+
+// Example Explanation
+// The value of X will be 2 and the value of Y will be 8. The XOR of 2 and 8 is 10.
 class StrangeEquality {
     StrangeEquality(int A)
     {
         int T = A;
         int MSB = (int)(Math.log(T)/Math.log(2));
+        /**
+     * log2(A) gives MSB 
+        why?
+        to find binary representation we divide number by 2 till 1 comes
+        8 msb 3
+        2 power 3 = 8 
+        2 power x = y   ===   log2(y) = x
+        log2(8) = 3(msb) 
+     * 
+     */
         for(int i=0; i<=MSB; i++) T = T^(1<<i); //flipbit
         System.out.print(T^(1<<(MSB+1)));
     }
@@ -10,3 +61,22 @@ class StrangeEquality {
         new StrangeEquality(5);
     }
 }
+/**
+ * ex: 5 
+    5: 0101
+    MSB = 2
+
+    to get number less than 5 i.e X we need to change all unset bits to set to get addition.
+     0101
+    +0010
+     0111 = 7 
+     flip all bit upto msb
+    similar
+    to get number greater than 5 i.e X we need to change msb+1 bit
+
+     0101
+    +1000
+     1101 = 13
+     2^8 = 10
+ * 
+ */
--- 5. Bit Manipulation/Code/reversebit.java
+++ 5. Bit Manipulation/Code/reversebit.java
@@ -1,3 +1,52 @@
+// Reverse Bits
+// Problem Description
+
+// Reverse the bits of an 32 bit unsigned integer A.
+
+
+
+// Problem Constraints
+// 0 <= A <= 232
+
+
+
+// Input Format
+// First and only argument of input contains an integer A.
+
+
+
+// Output Format
+// Return a single unsigned integer denoting the decimal value of reversed bits.
+
+
+
+// Example Input
+// Input 1:
+
+//  0
+// Input 2:
+
+//  3
+
+
+// Example Output
+// Output 1:
+
+//  0
+// Output 2:
+
+//  3221225472
+
+
+// Example Explanation
+// Explanation 1:
+
+//         00000000000000000000000000000000    
+// =>      00000000000000000000000000000000
+// Explanation 2:
+
+//         00000000000000000000000000000011    
+// =>      11000000000000000000000000000000
 class reversebit
 {
     static long checkbit(long N, long pos) { return ((N>>pos)&1L);}
@@ -5,14 +54,14 @@ class reversebit
     static long unsetbit(long N, long pos) { return N&(~(1L<<pos));}
     static long reverse(long N)
     {
-        long Msb = 31, Lsb = 0;
-        while(Msb>Lsb)
+        long Msb = 31, Lsb = 0;//msb bits 31
+        while(Msb>Lsb)//till msb pos is greater then lsb
         {
-            long Mbit = checkbit(N,Msb), Lbit = checkbit(N,Lsb);
-            if(Mbit!=Lbit)
+            long Mbit = checkbit(N,Msb), Lbit = checkbit(N,Lsb); //check msb and lsb 
+            if(Mbit!=Lbit) // we will only swap if msb and lsb bcz if same no swap is required.
             {
-                N = (Mbit==1L)?setbit(N,Lsb):unsetbit(N,Lsb);
-                N = (Lbit==1L)?setbit(N,Msb):unsetbit(N,Msb);
+                N = (Mbit==1L)?setbit(N,Lsb):unsetbit(N,Lsb); // if msb is 1 then unset it or viceversa
+                N = (Lbit==1L)?setbit(N,Msb):unsetbit(N,Msb); // if lsb is 1 then unset it or viceversa
             }
             Msb--;Lsb++;
         }
